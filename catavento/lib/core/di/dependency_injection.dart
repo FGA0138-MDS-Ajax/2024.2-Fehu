f5cdba6ff7cccc3f579260cd3a76600d1981b301
Pedro-Henrique3 <pedrohenriquefreirerodrigues@gmail.com>
2025-01-04 20:12:18 -0300

    [BUILD]: Inicializa dependências no GetIt

--- catavento/lib/core/di/dependency_injection.dart
+++ catavento/lib/core/di/dependency_injection.dart
@@ -1,9 +1,11 @@
 import 'package:catavento/core/di/dependency_injection.config.dart';
-import 'package:catavento/bloc/auth/auth_bloc.dart';
 import 'package:catavento/data/repositories/authentication_repository.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:catavento/bloc/auth/auth_bloc.dart';
+import 'package:catavento/bloc/login/login_bloc.dart';
+import 'package:catavento/bloc/registration/registration_bloc.dart';
 // import 'package:supabase_flutter/supabase_flutter.dart';
 // import 'package:gotrue/src/gotrue_client.dart';
 
@@ -17,4 +19,8 @@ void setupDependencies() {
       AuthenticationRepository(GoTrueClient()));
   getIt
       .registerSingleton<AuthBloc>(AuthBloc(getIt<AuthenticationRepository>()));
+  getIt.registerSingleton<LoginBloc>(
+      LoginBloc(getIt<AuthenticationRepository>()));
+  getIt.registerSingleton<RegistrationBloc>(
+      RegistrationBloc(getIt<AuthenticationRepository>()));
 }
